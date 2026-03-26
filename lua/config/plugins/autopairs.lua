50beb64ffaa3594ce5f9e98330c6c1bcb14febae
Alexandre Ramos <alr.melilloi@gmail.com>
2026-03-26 16:19:43 -0400

    added smear, autopair, lsp and telescope

---
+++ lua/config/plugins/autopairs.lua
@@ -0,0 +1,5 @@
+return {
+  "altermo/ultimate-autopair.nvim",
+  config = true,
+}
+
---
+++ lua/config/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.config").setup({
+      ensure_installed = "all",
+      sync_install = false, 
+      ignore_install = { "" }, -- List of parsers to ignore installing
+      highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = { "" }, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,
+
+      },
+      indent = { enable = true, disable = { "yaml" } },
+    })
+  end
+}
